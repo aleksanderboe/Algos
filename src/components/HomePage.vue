83bd0d33894bade2c5198c88382265bea2eeafe0
aleksanderboe <Aleksanderbo10@gmail.com>
2024-10-01 20:27:50 +0200

    add slider for length

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,27 +1,39 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch } from "vue";
 
 const canvas = ref(null);
 const ctx = ref(null);
-const array = ref([50, 20, 70, 10, 30, 90, 40, 60, 80, 100]);
+const array = ref([]);
 const boxWidth = ref(0);
 const sorting = ref(false);
-const speed = ref(1);
+const speed = ref(5);
+const size = ref(5);
+
+function generateRandomNumbers(count, min, max) {
+  return Array.from(
+    { length: count },
+    () => Math.floor(Math.random() * (max - min + 1)) + min
+  );
+}
+
+const createArray = () => {
+  array.value = generateRandomNumbers(size.value * 5, 10, 100);
+  boxWidth.value = canvas.value.width / array.value.length;
+  drawArray();
+};
 
 const drawArray = () => {
-  if (ctx.value) {
-    ctx.value.clearRect(0, 0, canvas.value.width, canvas.value.height);
-
-    array.value.forEach((value, i) => {
-      ctx.value.fillStyle = "blue";
-      ctx.value.fillRect(
-        i * boxWidth.value,
-        canvas.value.height - value,
-        boxWidth.value - 2,
-        value
-      );
-    });
-  }
+  if (!ctx.value) return;
+  ctx.value.clearRect(0, 0, canvas.value.width, canvas.value.height);
+  array.value.forEach((value, i) => {
+    ctx.value.fillStyle = "blue";
+    ctx.value.fillRect(
+      i * boxWidth.value,
+      canvas.value.height - value,
+      boxWidth.value - 2,
+      value
+    );
+  });
 };
 
 const bubbleSort = async () => {
@@ -33,9 +45,8 @@ const bubbleSort = async () => {
         const temp = array.value[j];
         array.value[j] = array.value[j + 1];
         array.value[j + 1] = temp;
-
         drawArray();
-        await new Promise((resolve) => setTimeout(resolve, 1000 / speed.value));
+        await new Promise((resolve) => setTimeout(resolve, 200 / speed.value));
       }
     }
   }
@@ -48,48 +59,72 @@ const startSorting = () => {
   }
 };
 
+watch(size, createArray);
+watch(speed, drawArray);
+
 onMounted(() => {
   ctx.value = canvas.value.getContext("2d");
-  boxWidth.value = canvas.value.width / array.value.length;
-  drawArray();
+  createArray();
 });
 </script>
 
 <template>
-  <div id="app" class="container">
-    <canvas ref="canvas" width="600" height="400"></canvas>
+  <div id="app" class="container my-4">
+    <h1 class="text-center mb-4">Sorting Algorithm Visualization</h1>
 
-    <div class="slidecontainer">
-      <label for="speedRange" class="form-label">Speed</label>
-
-      <input
-        type="range"
-        min="1"
-        max="10"
-        value="5"
-        class="form-range"
-        id="speedRange"
-        v-model="speed"
-      />
+    <div class="row">
+      <div class="col-12">
+        <canvas ref="canvas" class="w-100" width="800" height="200"></canvas>
+      </div>
     </div>
-    <p>{{ speed }}</p>
-    <button
-      type="button"
-      class="btn btn-primary"
-      @click="startSorting"
-      :disabled="sorting"
-    >
-      Start Bubble Sort
-    </button>
+
+    <div class="mt-4 row align-items-center">
+      <div class="col-2 text-end">
+        <span class="fs-3">Speed</span>
+      </div>
+      <div class="col-8">
+        <input
+          type="range"
+          min="1"
+          max="10"
+          v-model="speed"
+          class="form-range"
+          id="speedRange"
+        />
+      </div>
+      <div class="col-2">
+        <span class="fs-3">{{ speed }}</span>
+      </div>
+    </div>
+
+    <div class="mt-4 row align-items-center">
+      <div class="col-2 text-end">
+        <span class="fs-3">Size</span>
+      </div>
+      <div class="col-8">
+        <input
+          type="range"
+          min="1"
+          max="10"
+          v-model="size"
+          class="form-range"
+          id="sizeRange"
+        />
+      </div>
+      <div class="col-2">
+        <span class="fs-3">{{ size }}</span>
+      </div>
+    </div>
+
+    <div class="text-center mt-4">
+      <button
+        type="button"
+        class="btn btn-primary"
+        @click="startSorting"
+        :disabled="sorting"
+      >
+        Start
+      </button>
+    </div>
   </div>
 </template>
-
-<style scoped>
-canvas {
-  margin-bottom: 10px;
-}
-button {
-  padding: 10px 20px;
-  font-size: 16px;
-}
-</style>
